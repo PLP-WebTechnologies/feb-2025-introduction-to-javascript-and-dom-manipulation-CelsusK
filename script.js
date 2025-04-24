f5e2b2da15a6c82621fa07f3370091a6d7ca9bbf
Cassian Celsus <125782348+CelsusK@users.noreply.github.com>
2025-04-24 20:34:24 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const GoodwillDiv = document.getElementById("Goodwill")
+changeButton.addEventListener('click', () => {
+    GoodwillDiv.textContent = 'Hello from Goodwill!'
+    GoodwillDiv.style.color = 'blue';
+  });
+const HospitalDiv = document.getElementById("hospitalinfo")
+HospitalDiv.textContent = "Hello from Hospital!"
